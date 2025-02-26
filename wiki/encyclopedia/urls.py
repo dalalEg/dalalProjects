0a50aaef3fca41e35734f73d9af6c3bd060ebfa3
dalal.eg <dalal.eg@campus.technion.ac.il>
2025-02-26 15:27:55 +0200

    wiki

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 
 from . import views
+app_name = "encyclopedia"
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("error", views.error, name="error"),
+    path("add", views.add, name="add"),
+    path("<str:title>", views.entry, name="entry"),
+    path("wiki/<str:title>", views.entry, name="entry"),
+
 ]
--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,10 +1,32 @@
 from django.shortcuts import render
+from django import forms 
+from django.http import HttpResponseRedirect
+from django.shortcuts import render 
+import markdown2 as markdown
+from django.urls import reverse
+
+
+from . import models
 
 from . import util
 
 
 def index(request):
+
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+def entry(request, title):
+    content = util.get_entry(title)
+    if content is None:
+        return render(request, 'encyclopedia/error.html', {'entry': title})   
+    content = markdown.markdown(content)
+    return render(request, 'encyclopedia/entry.html',
+                   {'entry': title, 'content': content})
+
+def error(request):
+    return render(request, "encyclopedia/error.html")
+
+def add(request):
+    return render(request, "encyclopedia/add.html")
